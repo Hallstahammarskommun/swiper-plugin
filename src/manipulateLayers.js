a06922e0bfb5cae1bb8836ec585a1422b9245075
dahlalex <dahl.alex@hotmail.com>
2024-03-12 09:25:47 +0100

    Update src\manipulateLayer.js

--- src/manipulateLayers.js
+++ src/manipulateLayers.js
@@ -3,9 +3,13 @@ import stripJSONComments from './utils/stripjsoncomments';
 const ManipulateLayers = function ManipulateLayers(viewer, origoPath) {
   const _viewer = viewer;
   const _origoPath = origoPath;
-
   function createSwiperLayers() {
-    let url = window.location.href.split("intern")[0] + '\\' + _origoPath;
+    let url;
+    if (origoPath !== 'intern.json') {
+      url = origoPath;
+      url += '&swiper=ja';
+    } else {
+      url = window.location.href.split("intern")[0] + '\\' + _origoPath;
     if (window.location.hash) {
       const urlParams = viewer.permalink.parsePermalink(window.location.href);
       if (urlParams.map) {
@@ -15,8 +19,8 @@ const ManipulateLayers = function ManipulateLayers(viewer, origoPath) {
     const searchurlParams = new URLSearchParams(window.location.search);
     if (searchurlParams.has('mapStateId')) {
       url = (location.origin).concat(location.pathname).concat(_origoPath);
-    } 
-
+    }
+    }
     return fetch(url, {
       dataType: 'json' 
     })
@@ -28,7 +32,7 @@ const ManipulateLayers = function ManipulateLayers(viewer, origoPath) {
       let data;
       try {
         data = JSON.parse(stripped);
-       } catch (e) {
+      } catch (e) {
         const index = parseInt(e.message.split(' ').pop(), 10);
         if (index) {
           const row = stripped.substring(0, index).match(/^/gm).length;
